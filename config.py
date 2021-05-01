68a5835fb69e255df1766c2ed5c5228daaa4f06f
andrey18106 <andrey18106x@gmail.com>
2021-05-01 19:22:53 +0300

    Minor issue fixes

--- config.py
+++ config.py
@@ -2,17 +2,16 @@
 
 from dotenv import load_dotenv
 import os
-from pathlib import Path
 
-ROOT_DIR = Path('.')
-env_path = ROOT_DIR / '.env'
+ROOT_DIR = os.path.dirname(__file__)
+env_path = ROOT_DIR + '/.env'
 load_dotenv(dotenv_path=env_path)
 
 TOKEN = os.getenv('TOKEN')
 TOKEN_DEV = os.getenv('TOKEN_DEV')
 DEV_ID = os.getenv('DEV_ID')
-PATH_TO_DB = Path('.') / os.getenv('DB_NAME')
-PATH_TO_TRANSLATIONS = Path('.') / 'languages'
+PATH_TO_DB = ROOT_DIR + '/' + os.getenv('DB_NAME')
+PATH_TO_TRANSLATIONS = ROOT_DIR + '/languages'
 DEFAULT_LANG = 'en'
 
 LINGVOLIVE_API_KEY = os.getenv('LINGVOLIVE_API_KEY')
